8c5fd7f0e432780043669fbc3550ffe54c2f6fd7
Yurii Pylyponiuk <rutzi112@gmail.com>
2020-01-12 11:41:28 +0200

    popUp

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ function popUp(){
 	let takeCall = document.querySelectorAll('.number');
 	let closeBtn = document.querySelectorAll('.closeBtn');
 	let thanksBlock = document.querySelector('.thanksBlock');
-	let html = document.querySelector('html');
+	let body = document.querySelector('body');
 	let popupBlock = document.querySelector('.popupBlock');
 	let callbackOrder = document.querySelector('.callbackOrder');
 	let tnxbtn = document.querySelectorAll('.tnxbtn');
 	let fastAppliBtn = document.querySelector('.fastAppliBtn');
 	let detailOrderBtn = document.querySelector('.detailOrderBtn');
 	let showfastPopup = document.querySelector('.showfastPopup');
+
+
 	for(let i=0;i<takeCall.length;i++){
 		takeCall[i].addEventListener('click', call);
 	}
@@ -34,19 +36,23 @@ function popUp(){
 		// 	thanksBlock.classList.toggle('active');
 		// }
 		remove(e);
-			thanksBlock.classList.toggle('active');
+		body.classList.toggle('scroll');
+		thanksBlock.classList.toggle('active');
 	}
 	function showfastAppl(e){
 		e.preventDefault();
+		body.classList.toggle('scroll');
 		showfastPopup.classList.toggle('active');
 	}
 	function call(e){
 		e.preventDefault();
+		body.classList.toggle('scroll');
 		callbackOrder.classList.toggle('active');
 	}
 	function remove(e){
 		e.preventDefault();
 		e.target.parentElement.classList.remove('active');
+		body.classList.remove('scroll');
 	}
 }
 popUp();
